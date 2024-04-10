686289eabffdb0aedfb9facef958b98912fdf08a
Divesh-yudiz <divesh.adole@yudiz.com>
2024-04-10 18:02:53 +0530

    images deleted

--- src/js/three.js
+++ src/js/three.js
@@ -4,12 +4,6 @@ import * as THREE from 'three';
 
 import fragment from '../shaders/fragment.glsl';
 import vertex from '../shaders/vertex.glsl';
-// import BLUE from '/Colors/blue.png'
-// import ORANGE from '/Colors/orange.png'
-// import RED from '/Colors/red.png'
-// import WHITE from '/Colors/white.png'
-// import YELLOW from '/Colors/yellow.png'
-// import GREEN from '/Colors/green.png'
 
 const device = {
   width: window.innerWidth,
